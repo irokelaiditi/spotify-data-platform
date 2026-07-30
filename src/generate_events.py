afe517ac886dabef7645e777024ac0a0261a3102
Iro Kelaiditi <127994947+irokelaiditi@users.noreply.github.com>
2026-07-30 20:17:23 +0000

    Expand listening event schema

--- src/generate_events.py
+++ src/generate_events.py
@@ -4,12 +4,32 @@ print("Spotify event generator started")
 import json
 import random
 from pathlib import Path
+from datetime import datetime, timedelta, timezone
 
 event_types = [
     "track_played",
     "track_paused",
     "track_skipped",
     "track_completed",
+]
+
+device_types = [
+    "mobile",
+    "desktop",
+    "tablet",
+]
+
+countries = [
+    "GR",
+    "UK",
+    "DE",
+    "FR",
+    "IT",
+]
+
+subscription_types = [
+    "free",
+    "premium",
 ]
 
 event = {
@@ -21,12 +41,19 @@ event = {
 print(event)
 
 events = []
+start_time = datetime.now(timezone.utc) - timedelta(days=30)
 
 for _ in range(10_000):
     event = {
         "user_id": random.randint(1, 1000),
         "track_id": random.randint(1, 500),
         "event_type": random.choice(event_types),
+        "timestamp": (
+        start_time + timedelta(seconds=random.randint(0, 30 * 24 * 60 * 60))).isoformat(),
+        "device_type": random.choice(device_types),
+        "country": random.choice(countries),
+        "subscription_type": random.choice(subscription_types),
+        "listening_duration_seconds": random.randint(10, 600),
     }
 
     events.append(event)
